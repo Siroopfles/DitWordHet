0da5bbdd92f963bfbd7adfa1ef015dfec75173a9
Djes Samuels <djdjes13@gmail.com>
2025-04-01 21:02:07 +0200

    fix: simplify eslint config and remove @eslint/js dependency

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,13 @@
-import js from '@eslint/js';
 import tsPlugin from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import prettierConfig from 'eslint-config-prettier';
 
 export default [
-  js.configs.recommended,
   {
     files: ["**/*.js", "**/*.ts", "**/*.mjs"],
     languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
       parser: tsParser,
       parserOptions: {
         project: './tsconfig.json',
